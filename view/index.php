d99ee8dd2e77040f2893f1af82c3638341f1725d
kalanna79 <natacha@boudetnature.com>
2017-03-28 08:19:39 +0200

    add chapters and footer - del contact

--- view/index.php
+++ view/index.php
@@ -8,66 +8,107 @@
     
     ?>
 
+<!DOCTYPE HTML>
 <html>
 <head>
     <title>Billet pour l'Alaska</title>
     <meta charset="UTF-8">
     
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<link href="bootstrap/css/personal.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
 </head>
 
 <body>
-    <main>
-        <h1>Billet pour l'Alaska</h1>
-        
+    <main class="container">
         <!-- partie de gauche avec présentation auteur & form de contact -->
-        <section>
-            <article> texte présentation auteur</article>
-           
-            <div>
-                <h2>Me contacter</h2>
-                <form>
-                    <input type="text" placeholder="Prénom" name="prenom">
-                    <input type="text" placeholder="Nom" name="nom"><br>
-                    <input type="email" placeholder="Votre mail" name="mail"><br>
-                    <textarea name="message" placeholder="Votre message" rows="5" cols="50"></textarea><br>
-                    <input type="submit" name="envoyer" value="Envoyer">
-                </form>
-            </div>
+		<div class="row">
+			<div class="col-md-9">
+				<h1>Billet pour l'Alaska</h1>
+			</div>
+			<div class="col-md-3 icon-menu">
+				<a href="#" class="fa fa-user-plus fa-lg btn-lg valign"></a>
+				<a href="#" class="fa fa-sign-in fa-lg btn-lg valign"></a>
+				<a href="#" class="fa fa-envelope-o fa-lg btn-lg valign"></a>
+			</div>
+		</div>
+		<section class="col-md-6 presentation">
+			
+			
+			<article>
+				
+				<img src="img/rafting-1377057_640.jpg" width="100%" class="valign">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper sem erat, in lobortis enim viverra et. Nunc sit amet lacus consequat, congue lacus ornare, lacinia lorem. Suspendisse sit amet ligula imperdiet, commodo eros ac, molestie tortor. Praesent id velit rutrum libero mollis maximus eu ut quam. Quisque dignissim massa metus, vel maximus sem consectetur non. Nunc at enim leo. Sed dapibus ligula leo, id iaculis arcu mollis eget.
+				
+				Praesent dictum nibh ac ultricies eleifend. Sed eget pulvinar lacus. Suspendisse dictum dictum lacus bibendum imperdiet. Sed ut nisi dignissim enim tincidunt aliquet sed sed velit. Aenean vitae lobortis nulla, non elementum nibh. Vestibulum a fermentum urna. Integer sed sodales metus. Nulla tincidunt arcu a ex ultricies, at ultrices velit sollicitudin. Proin tempus pretium orci sit amet iaculis. Duis eget pellentesque sem. In eu faucibus nunc. Sed posuere, nunc et imperdiet eleifend, enim sapien malesuada enim, in sodales purus libero sit amet turpis.
+				
+			</article>
+			
+            
         </section>
-        
-        <!-- partie de droite avec Table des matières comprenant num chap + titre chap + date ajout + 300 car et Lire la suite - en sticky, lien vers les commentaires généraux -->
-        <section>
+        <!-- partie de droite avec Table des matières comprenant num chap + titre chap + date ajout + 300 car et Lire la suite -->
+        <section class="col-xs-12 col-md-6 table_mat">
             <h2>Table des matières</h2>
-            <div>
-                <a href="#">Chapitre 1</a>
-                <time datetime="2017-04-04">4 avril 2017</time>
-                <p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his quidam ex Catones his reperiuntur omittamus qui eos communis eos quidam magnificentia virtutem autem bonos sermonisque docti contenta qui nusquam eos reperiuntur eos autem Galos Catones Philos vita autem quidam vitae virosque nostri eos ut.</p>
-                <a href="#">... Lire la suite ...</a>
-            </div>
-            <div>
-                <a href="#">Chapitre 2</a>
-                <time datetime="2017-04-04">4 avril 2017</time>
-                <p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his quidam ex Catones his reperiuntur omittamus qui eos communis eos quidam magnificentia virtutem autem bonos sermonisque docti contenta qui nusquam eos reperiuntur eos autem Galos Catones Philos vita autem quidam vitae virosque nostri eos ut.</p>
-                <a href="#">... Lire la suite ...</a>
-            </div>
-            <div>
-                <a href="#">Chapitre 2</a>
-                <time datetime="2017-04-04">4 avril 2017</time>
-                <p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his quidam ex Catones his reperiuntur omittamus qui eos communis eos quidam magnificentia virtutem autem bonos sermonisque docti contenta qui nusquam eos reperiuntur eos autem Galos Catones Philos vita autem quidam vitae virosque nostri eos ut.</p>
-                <a href="#">... Lire la suite ...</a>
-            </div>
+			<hr>
+			<div class="row">
+				<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 dropdown">
+					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
+					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
+					</span>
+					
+					<div class="dropdown-menu"> <p>Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his. <a href="#">... Lire la suite ...</a> </p>
+						</div>
+					
+				</div>
+			</div>
+			
+			<div class=" col-xs-6 dropdown">
+					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
+					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
+					</span>
+		
+				<div class="dropdown-menu"><p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>>
+					<a href="#">... Lire la suite ...</a></div>
+	
+			</div>
+			
+			<div class="dropdown">
+					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
+					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
+					</span>
+		
+				<div class="dropdown-menu"><p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
+					<a href="#">... Lire la suite ...</a></div>
+	
+			</div>
+			
+			<div class="dropdown">
+					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
+					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
+					</span>
+		
+				<div class="dropdown-menu"><p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
+					<a href="#">... Lire la suite ...</a></div>
+	
+			</div>
+			
         </section>
-        
-        <!-- col droite pour icones liens connexion/inscription & réseaux sociaux  -->
-        <aside>
-            <a href="#">S'inscrire</a>
-            <a href="#">Se connecter</a>
-            <a href="#">Facebook</a>
-            <a href="#">Google+</a>
-            <a href="#">Twitter</a>
-            <a href="#">Pinterest</a>
-        </aside>
-        <footer>Copyright Jean Forteroche 2017</footer>
+	
+		<footer>
+			<div class="row">
+				<div class="col-md-6"><span>Copyright Jean Forteroche 2017</span></div>
+				<div class="col-md-6">
+					<a href="#" class="fa fa-facebook-square fa-lg btn-md"></a>
+					<a href="#" class="fa fa-google-plus-square fa-lg btn-md"></a>
+					<a href="#" class="fa fa-twitter-square fa-lg btn-md"></a>
+					<a href="#" class="fa fa-pinterest-square fa-lg btn-md"></a>
+				</div>
+		</footer>
     </main>
+	
+	
+	<script src="bootstrap/js/query.js"></script>
+	<script src="bootstrap/js/bootstrap.js"></script>
 </body>
 </html>
