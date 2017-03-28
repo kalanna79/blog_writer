3340d8adbff455baca16fc671a3b54043bf084aa
kalanna79 <natacha@boudetnature.com>
2017-03-28 08:20:49 +0200

    add view contact

---
+++ view/contact.php
@@ -0,0 +1,22 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 25/03/2017
+     * Time: 16:40
+     */
+    ?>
+
+<!DOCTYPE HTML>
+<html>
+    <div>
+                <h3>Me contacter</h3>
+                <form>
+                    <input type="text" placeholder="Prénom" name="prenom">
+                    <input type="text" placeholder="Nom" name="nom"><br>
+                    <input type="email" placeholder="Votre mail" name="mail"><br>
+                    <textarea name="message" placeholder="Votre message" rows="5"></textarea><br>
+                    <input type="submit" name="envoyer" value="Envoyer">
+                </form>
+            </div>
+</html>
