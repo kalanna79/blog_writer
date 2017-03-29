9fe3583c934844f154298424a35d66472f2775d6
kalanna79 <natacha@boudetnature.com>
2017-03-29 07:35:58 +0200

    modify tab_mat and presentation

--- view/index.php
+++ view/index.php
@@ -33,15 +33,17 @@
 				<a href="#" class="fa fa-envelope-o fa-lg btn-lg valign"></a>
 			</div>
 		</div>
+		<div class="row principal">
 		<section class="col-md-6 presentation">
 			
 			
 			<article>
 				
 				<img src="img/rafting-1377057_640.jpg" width="100%" class="valign">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper sem erat, in lobortis enim viverra et. Nunc sit amet lacus consequat, congue lacus ornare, lacinia lorem. Suspendisse sit amet ligula imperdiet, commodo eros ac, molestie tortor. Praesent id velit rutrum libero mollis maximus eu ut quam. Quisque dignissim massa metus, vel maximus sem consectetur non. Nunc at enim leo. Sed dapibus ligula leo, id iaculis arcu mollis eget.
 				
-				Praesent dictum nibh ac ultricies eleifend. Sed eget pulvinar lacus. Suspendisse dictum dictum lacus bibendum imperdiet. Sed ut nisi dignissim enim tincidunt aliquet sed sed velit. Aenean vitae lobortis nulla, non elementum nibh. Vestibulum a fermentum urna. Integer sed sodales metus. Nulla tincidunt arcu a ex ultricies, at ultrices velit sollicitudin. Proin tempus pretium orci sit amet iaculis. Duis eget pellentesque sem. In eu faucibus nunc. Sed posuere, nunc et imperdiet eleifend, enim sapien malesuada enim, in sodales purus libero sit amet turpis.
+				<div class="colonne">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper sem erat, in lobortis enim viverra et. Nunc sit amet lacus consequat, congue lacus ornare, lacinia lorem. Suspendisse sit amet ligula imperdiet, commodo eros ac, molestie tortor. Praesent id velit rutrum libero mollis maximus eu ut quam. Quisque dignissim massa metus, vel maximus sem consectetur non. Nunc at enim leo. Sed dapibus ligula leo, id iaculis arcu mollis eget.
+				
+									 Praesent dictum nibh ac ultricies eleifend. Sed eget pulvinar lacus. Suspendisse dictum dictum lacus bibendum imperdiet. Sed ut nisi dignissim enim tincidunt aliquet sed sed velit. Aenean vitae lobortis nulla, non elementum nibh. Vestibulum a fermentum urna. Integer sed sodales metus. Nulla tincidunt arcu a ex ultricies, at ultrices velit sollicitudin. Proin tempus pretium orci sit amet iaculis. Duis eget pellentesque sem. In eu faucibus nunc. Sed posuere, nunc et imperdiet eleifend, enim sapien malesuada enim, in sodales purus libero sit amet turpis.</div>
 				
 			</article>
 			
@@ -51,64 +53,153 @@
         <section class="col-xs-12 col-md-6 table_mat">
             <h2>Table des matières</h2>
 			<hr>
-			<div class="row">
-				<div class="col-xs-6 col-sm-6 col-md-4 col-lg-4 dropdown">
-					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
-					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
-					</span>
-					
-					<div class="dropdown-menu"> <p>Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his. <a href="#">... Lire la suite ...</a> </p>
+			
+				<div class="row user-row">
+			
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+						<div class="row chap-box-icon">
+							<strong class="chap">Chap 1</strong><br>
+							<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+								<abbr><time datetime="2017-04-04">31/03/2017</time></abbr>
+							</div>
+							<div class="col-xs-3 col-sm-3 col-md-1 col-lg-1 dropdown-user" data-for=".chap1">
+								<i class="glyphicon glyphicon-chevron-down text-muted"></i>
+							</div>
 						</div>
+					</div>
+			
+			
+					<div class="row user-infos chap1">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<div class="panel panel-primary">
+								<div class="panel-body">
+									<div class="row">
+								
+								
+										<div>
+											<p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
+											<a href="#">... Lire la suite ...</a>
+								
+										</div>
+									</div>
+								</div>
 					
+							</div>
+						</div>
+					</div>
+			
+			
+			
+			
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+						<div class="row chap-box-icon">
+							<strong class="chap">Chap 2</strong><br>
+							<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+								<abbr><time datetime="2017-04-04">06/04/2017</time></abbr>
+							</div>
+							<div class="col-xs-3 col-sm-3 col-md-1 col-lg-1 dropdown-user" data-for=".chap2">
+								<i class="glyphicon glyphicon-chevron-down text-muted"></i>
+							</div>
+						</div>
+					</div>
+			
+					<div class="row user-infos chap2">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<div class="panel panel-primary">
+								<div class="panel-body">
+									<div class="row">
+								
+								
+										<div class=" col-md-12 col-lg-12">
+											<p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
+											<a href="#">... Lire la suite ...</a>
+										</div>
+									</div>
+								</div>
+					
+							</div>
+						</div>
+					</div>
+			
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+						<div class="row chap-box-icon">
+							<strong class="chap">Chap 3</strong><br>
+							<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+								<abbr><time datetime="2017-04-04">15/04/2017</time></abbr>
+							</div>
+							<div class="col-xs-3 col-sm-3 col-md-1 col-lg-1 dropdown-user" data-for=".chap3">
+								<i class="glyphicon glyphicon-chevron-down text-muted"></i>
+							</div>
+						</div>
+					</div>
+			
+					<div class="row user-infos chap3">
+						<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+							<div class="panel panel-primary">
+								<div class="panel-body">
+									<div class="row">
+								
+								
+										<div class=" col-md-12 col-lg-12">
+											<p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
+											<a href="#">... Lire la suite ...</a>
+										</div>
+									</div>
+								</div>
+					
+							</div>
+						</div>
+					</div>
+			
+					<div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+						<div class="row chap-box-icon">
+							<strong class="chap">Chap 4</strong><br>
+							<div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+								<abbr><time datetime="2017-04-04">21/04/2017</time></abbr>
+							</div>
+							<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1 dropdown-user" data-for=".chap4">
+								<span><i class="glyphicon glyphicon-chevron-down text-muted"></i></span>
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-			
-			<div class=" col-xs-6 dropdown">
-					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
-					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
-					</span>
 		
-				<div class="dropdown-menu"><p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>>
-					<a href="#">... Lire la suite ...</a></div>
-	
-			</div>
+				<div class="row user-infos chap4">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+						<div class="panel panel-primary">
+							<div class="panel-body">
+								<div class="row">
+							
+							
+									<div class=" col-md-12 col-lg-12">
+										<p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
+										<a href="#">... Lire la suite ...</a>
+									</div>
+								</div>
+							</div>
+				
+						</div>
+					</div>
+				</div>
 			
-			<div class="dropdown">
-					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
-					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
-					</span>
-		
-				<div class="dropdown-menu"><p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
-					<a href="#">... Lire la suite ...</a></div>
-	
-			</div>
-			
-			<div class="dropdown">
-					<span class="chap-box-icon dropdown-toggle" data-toggle="dropdown"> <a href="#">Chap 1</a><br>
-					<abbr><time datetime="2017-04-04">4 avril 2017</time></abbr>
-					</span>
-		
-				<div class="dropdown-menu"><p> Eam eam Philos qui omnino nostri habentur nusquam vita communis docti vita vita habentur Scipiones vita Catones virosque est interpretemur bonos Galos ut Philos nusquam ut vitae virtutem virosque bonos reperiuntur nec his qui eos vita docti qui virosque qui qui docti eos omittamus Scipiones eos qui numeremus eos Paulos bonos ex magnificentia metiamur bonos metiamur communis numeremus Paulos Catones nusquam qui sermonisque communis his.</p>
-					<a href="#">... Lire la suite ...</a></div>
-	
-			</div>
 			
         </section>
-	
-		<footer>
-			<div class="row">
-				<div class="col-md-6"><span>Copyright Jean Forteroche 2017</span></div>
-				<div class="col-md-6">
-					<a href="#" class="fa fa-facebook-square fa-lg btn-md"></a>
-					<a href="#" class="fa fa-google-plus-square fa-lg btn-md"></a>
-					<a href="#" class="fa fa-twitter-square fa-lg btn-md"></a>
-					<a href="#" class="fa fa-pinterest-square fa-lg btn-md"></a>
-				</div>
-		</footer>
+		</div>
+		
     </main>
-	
+	<footer>
+		<div class="row">
+			<div class="col-md-6"><span>Copyright Jean Forteroche 2017</span></div>
+			<div class="col-md-6">
+				<a href="#" class="fa fa-facebook-square fa-lg btn-md"></a>
+				<a href="#" class="fa fa-google-plus-square fa-lg btn-md"></a>
+				<a href="#" class="fa fa-twitter-square fa-lg btn-md"></a>
+				<a href="#" class="fa fa-pinterest-square fa-lg btn-md"></a>
+			</div>
+	</footer>
 	
 	<script src="bootstrap/js/query.js"></script>
 	<script src="bootstrap/js/bootstrap.js"></script>
+	<script src="bootstrap/js/personal.js"></script>
 </body>
 </html>
