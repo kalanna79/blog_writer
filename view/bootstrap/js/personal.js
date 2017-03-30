6c8e2fd6e8e3852c81f22d1f14003417c9b0f691
kalanna79 <natacha@boudetnature.com>
2017-03-30 10:10:06 +0200

    modify showButton in tab_mat

--- view/bootstrap/js/personal.js
+++ view/bootstrap/js/personal.js
@@ -1,30 +1,10 @@
-$(document).ready(function() {
-    var panels = $('.user-infos');
-    var panelsButton = $('.dropdown-user');
-    panels.hide();
+//visible or hidden excerpts
 
-    //Click dropdown
-    panelsButton.click(function() {
-        //get data-for attribute
-        var dataFor = $(this).attr('data-for');
-        var idFor = $(dataFor);
-
-        //current button
-        var currentButton = $(this);
-        idFor.slideToggle(400, function() {
-            //Completed slidetoggle
-            if(idFor.is(':visible'))
-            {
-                currentButton.html('<i class="glyphicon glyphicon-chevron-up text-muted"></i>');
-            }
-            else
-            {
-                currentButton.html('<i class="glyphicon glyphicon-chevron-down text-muted"></i>');
-            }
-        })
-    });
-
-
-    $('[data-toggle="tooltip"]').tooltip();
-
+$('.showButton').mouseover(function() {
+    var nb = $(this).attr('id');
+    $('#details-'+nb).show();
 });
+
+$('.showButton').mouseout(function() {
+    $('.details-chap').hide();
+})
