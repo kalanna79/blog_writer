e71b320d4cca379bfce4e56e6867d5e674b3c1b1
kalanna79 <natacha@boudetnature.com>
2017-03-30 10:10:21 +0200

    create chapter view

--- view/chapter.php
+++ view/chapter.php
@@ -5,3 +5,97 @@
      * Date: 29/03/2017
      * Time: 07:36
      */
+    
+    ?>
+
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Billet pour l'Alaska</title>
+    <meta charset="UTF-8">
+    
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="bootstrap/css/personal.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Space+Mono" rel="stylesheet">
+</head>
+
+<body>
+    <main class="container">
+        <!-- partie de gauche avec présentation auteur & form de contact -->
+        <div class="row">
+            <div class="col-xs-12 col-md-offset-1 col-md-8 ">
+                <h1>Billet pour l'Alaska</h1>
+            </div>
+            <div class="col-md-2 icon-menu">
+                <a href="#" class="fa fa-user-plus fa-lg btn-lg valign"></a>
+                <a href="#" class="fa fa-sign-in fa-lg btn-lg valign"></a>
+                <a href="#" class="fa fa-envelope-o fa-lg btn-lg valign"></a>
+            </div>
+        </div>
+		<div class=" col-xs-12 col-md-offset-1 col-md-10">
+        <div class="row">
+            <nav class="col-md-2">
+                <ul class="nav flex-column nav-tabs-justified">
+                    <li class="nav-item">
+						<a class="navbar-link" href="#">chapter 1</a>
+                    </li>
+					<li class="nav-item">
+						<a class="navbar-link"" href="#">chapter 2</a>
+					</li>
+					<li class="nav-item">
+						<a class="navbar-link" href="#">chapter 3</a>
+					</li>
+					<li class="nav-item">
+						<a class="navbar-link" href="#">chapter 4</a>
+					</li>
+					<li class="nav-item">
+						<a class="navbar-link" href="index.php">Accueil</a>
+					</li>
+                </ul>
+            </nav>
+            
+            <section class="col-xs-12 col-md-10 principal" style="position: relative">
+				<div class="row" style="text-align: center;">
+					<img src="img/rafting-1377057_640.jpg" width="55%" class="valign" ">
+				</div>
+				
+						
+						<div class="row">
+							
+							<div class="col-md-12 book"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper sem erat, in lobortis enim viverra et. Nunc sit amet lacus consequat, congue lacus ornare, lacinia lorem. Suspendisse sit amet ligula imperdiet, commodo eros ac, molestie tortor. Praesent id velit rutrum libero mollis maximus eu ut quam. Quisque dignissim massa metus, vel maximus sem consectetur non. Nunc at enim leo. Sed dapibus ligula leo, id iaculis arcu mollis eget.
+        
+                                         Praesent dictum nibh ac ultricies eleifend. Sed eget pulvinar lacus. Suspendisse dictum dictum lacus bibendum imperdiet. Sed ut nisi dignissim enim tincidunt aliquet sed sed velit. Aenean vitae lobortis nulla, non elementum nibh. Vestibulum a fermentum urna. Integer sed sodales metus. Nulla tincidunt arcu a ex ultricies, at ultrices velit sollicitudin. Proin tempus pretium orci sit amet iaculis. Duis eget pellentesque sem. In eu faucibus nunc. Sed posuere, nunc et imperdiet eleifend, enim sapien malesuada enim, in sodales purus libero sit amet turpis.
+						
+							 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper sem erat, in lobortis enim viverra et. Nunc sit amet lacus consequat, congue lacus ornare, lacinia lorem. Suspendisse sit amet ligula imperdiet, commodo eros ac, molestie tortor. Praesent id velit rutrum libero mollis maximus eu ut quam. Quisque dignissim massa metus, vel maximus sem consectetur non. Nunc at enim leo. Sed dapibus ligula leo, id iaculis arcu mollis eget.
+						
+							 Praesent dictum nibh ac ultricies eleifend. Sed eget pulvinar lacus. Suspendisse dictum dictum lacus bibendum imperdiet. Sed ut nisi dignissim enim tincidunt aliquet sed sed velit. Aenean vitae lobortis nulla, non elementum nibh. Vestibulum a fermentum urna. Integer sed sodales metus. Nulla tincidunt arcu a ex ultricies, at ultrices velit sollicitudin. Proin tempus pretium orci sit amet iaculis. Duis eget pellentesque sem. In eu faucibus nunc. Sed posuere, nunc et imperdiet eleifend, enim sapien malesuada enim, in sodales purus libero sit amet turpis.
+						
+							 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ullamcorper sem erat, in lobortis enim viverra et. Nunc sit amet lacus consequat, congue lacus ornare, lacinia lorem. Suspendisse sit amet ligula imperdiet, commodo eros ac, molestie tortor. Praesent id velit rutrum libero mollis maximus eu ut quam. Quisque dignissim massa metus, vel maximus sem consectetur non. Nunc at enim leo. Sed dapibus ligula leo, id iaculis arcu mollis eget.
+						
+							 Praesent dictum nibh ac ultricies eleifend. Sed eget pulvinar lacus. Suspendisse dictum dictum lacus bibendum imperdiet. Sed ut nisi dignissim enim tincidunt aliquet sed sed velit. Aenean vitae lobortis nulla, non elementum nibh. Vestibulum a fermentum urna. Integer sed sodales metus. Nulla tincidunt arcu a ex ultricies, at ultrices velit sollicitudin. Proin tempus pretium orci sit amet iaculis. Duis eget pellentesque sem. In eu faucibus nunc. Sed posuere, nunc et imperdiet eleifend, enim sapien malesuada enim, in sodales purus libero sit amet turpis.
+								</p>
+							</div>
+							</div>
+						</div>
+            </section>
+			
+        </div>
+
+    </main>
+    <footer>
+        <div class="row">
+            <div class="col-md-offset-1 col-md-7"><span>Copyright Jean Forteroche 2017</span></div>
+            <div class="col-md-2">
+                <a href="#" class="fa fa-facebook-square fa-lg btn-md"></a>
+                <a href="#" class="fa fa-google-plus-square fa-lg btn-md"></a>
+                <a href="#" class="fa fa-twitter-square fa-lg btn-md"></a>
+                <a href="#" class="fa fa-pinterest-square fa-lg btn-md"></a>
+            </div>
+    </footer>
+    
+    <script src="bootstrap/js/query.js"></script>
+    <script src="bootstrap/js/bootstrap.js"></script>
+    <script src="bootstrap/js/personal.js"></script>
+</body>
+</html>
