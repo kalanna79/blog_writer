3268dc4eef7c194ad710350211fbcfee4dfde71a
kalanna79 <natacha@boudetnature.com>
2017-04-12 15:32:36 +0200

    show the excerpt of the last chapter by default

--- bootstrap/js/personal.js
+++ bootstrap/js/personal.js
@@ -2,9 +2,17 @@
 
 $('.showButton').mouseover(function() {
     var nb = $(this).attr('id');
+    $('.details-chap').last().hide();
     $('#details-'+nb).show();
 });
 
 $('.showButton').mouseout(function() {
     $('.details-chap').hide();
+    $('.details-chap').last().show();
+})
+
+$('.showButton').ready(function() {
+   // $('h3').last().add('<span id="new">NEW</span>').prependTo(document.getElementsByClassName('details-chap'));
+
+    $('.details-chap').last().show();
 })
