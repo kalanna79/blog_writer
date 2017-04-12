cf98ed32127ef6f4575b989b1c9da9938ee77b18
kalanna79 <natacha@boudetnature.com>
2017-04-12 15:34:51 +0200

    create the file for function of summary tab

---
+++ model/index.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 11/04/2017
+     * Time: 18:35
+     */
+    
+    $chapter = $chap['id'];
+    
+    function nbchap($chapter) {
+        $nb = count($chapter);
+        if ($nb > 16) {
+            
+        }
+    }
