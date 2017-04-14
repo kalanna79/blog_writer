01277b139672644169a5841aaae9802e2a5ac766
kalanna79 <natacha@boudetnature.com>
2017-04-14 17:24:32 +0200

    create Inscription file with includes

---
+++ controller/inscription.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 14/04/2017
+     * Time: 08:29
+     */
+    
+    include('../config.php');
+    include (VIEW.'header.php');
+    include (VIEW.'inscription.php');
+    include (VIEW.'footer.php');
