1515b8550293a08ffcbfaab69d99a869407ade24
kalanna79 <natacha@boudetnature.com>
2017-04-14 08:24:58 +0200

    create view connection/inscription

---
+++ view/inscription.php
@@ -0,0 +1,34 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 13/04/2017
+     * Time: 22:33
+     */
+    include('../config.php');
+    include(VIEW . 'header.php');
+    
+    ?>
+
+<div class="row principal">
+    <section class="col-md-6 presentation">
+        <h3>Se connecter</h3>
+        <p>Vous avez déjà un compte ? Connectez-vous.</p>
+        <form action="" method="post">
+            <label for="pseudo">Pseudo</label> <br>
+            <input type="text" name="pseudo"><br>
+            <label for="pwd">Mot de passe</label> <br>
+            <input type="text" name="pwd"><br><br>
+            <input type="submit" name="Se connecter">
+        </form>
+    
+    </section>
+    <section class="coll-md-6 presentation"></section>
+        
+</div>
+    
+</div>
+
+
+
+
