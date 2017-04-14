575eae558ab41e22a53d28971cec45422542b67e
kalanna79 <natacha@boudetnature.com>
2017-04-14 08:25:14 +0200

    create form comments

---
+++ view/comments.php
@@ -0,0 +1,33 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 13/04/2017
+     * Time: 19:12
+     */
+    
+    
+    
+    ?>
+
+<section class="col-xs-12 col-md-offset-2 col-md-8 principal">
+    <div class="row">
+        <div class="comments">
+            <i class="fa fa-comments fa-3x" aria-hidden="true"></i>
+        </div>
+        <div class="row">
+        
+        </div>
+        <div class="row">
+            <p>Déposez un commentaire : </p>
+            <form action="" method="post">
+                <label for="pseudo">Pseudo</label><br>
+                <input type="text" name="pseudo"> <br>
+                <label for="email">Email</label><br>
+                <input type="text" name="email"> <br>
+                <label for="titre">Titre de votre message</label><br>
+                <input type="text" name="titre"> <br><br>
+                <textarea name="message" cols="115" rows="5">Votre message</textarea> <br><br>
+                <input type="submit" value="Envoyer" name="submit">
+
+</section>
