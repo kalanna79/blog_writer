53d53b7a9bd5c3c82ddae0b235032747dce572a7
kalanna79 <natacha@boudetnature.com>
2017-04-14 17:23:54 +0200

    delete link to Bdd and extend class to BddManager

--- model/ChapterManager.php
+++ model/ChapterManager.php
@@ -6,22 +6,8 @@
      * Time: 08:03
      */
     
-    class ChapterManager
+    class ChapterManager extends BddManager
     {
-        private $_db;
-        private $_hostname = 'localhost';
-        private $_login = 'root';
-        private $_pwd = 'root';
-        private $_dbname = 'blogwriter';
-        
-        public function __construct()
-        {
-            $db = new PDO('mysql:hostname='. $this->_hostname.';dbname='.$this->_dbname.';charset=utf8',
-                          $this->_login, $this->_pwd);
-            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-            $this->_db = $db;
-        }
-        
         //afficher tous les livres
         public function Tab_matieres()
         {
