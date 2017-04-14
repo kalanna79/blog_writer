603404105823bfe8548247df010dcb139500959a
kalanna79 <natacha@boudetnature.com>
2017-04-14 17:26:21 +0200

    create view for inscription and complete view for connection

--- view/inscription.php
+++ view/inscription.php
@@ -5,8 +5,6 @@
      * Date: 13/04/2017
      * Time: 22:33
      */
-    include('../config.php');
-    include(VIEW . 'header.php');
     
     ?>
 
@@ -18,12 +16,32 @@
             <label for="pseudo">Pseudo</label> <br>
             <input type="text" name="pseudo"><br>
             <label for="pwd">Mot de passe</label> <br>
-            <input type="text" name="pwd"><br><br>
-            <input type="submit" name="Se connecter">
-        </form>
-    
+            <input type="text" name="pwd"><br>
+            <input type="checkbox" name="souvenir"> Se souvenir de moi <br><br>
+            <input type="submit" name="connect" value="Se connecter">
+        </form><br>
+        <a href="#">Mot de passe oublié ?</a>
+      
     </section>
-    <section class="coll-md-6 presentation"></section>
+    <section class="col-md-6 presentation">
+        <h3>S'inscrire</h3>
+        <p>Inscrivez-vous pour déposer des commentaires et retrouvez votre avancée de lecture</p>
+        <form action="" method="post">
+            <label for="prenom">Prénom</label> <br>
+            <input type="text" name="prenom"><br>
+            <label for="nom">Nom</label> <br>
+            <input type="text" name="nom"><br>
+            <label for="pseudo">Pseudo</label> <br>
+            <input type="text" name="pseudo"><br>
+            <label for="email">Email</label> <br>
+            <input type="text" name="email"><br>
+            <label for="pwd">Mot de passe</label> <br>
+            <input type="text" name="pwd"><br>
+            <label for="pwd2">Répétez votre mot de passe</label> <br>
+            <input type="text" name="pwd2"><br><br>
+            <input type="submit" name="inscription" value="Créer mon compte">
+        </form>
+    </section>
         
 </div>
     
