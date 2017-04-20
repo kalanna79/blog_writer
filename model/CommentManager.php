3217c56c0c47c4d39baf29b9afdd1373e72203bf
kalanna79 <natacha@boudetnature.com>
2017-04-20 20:14:16 +0200

    create CommentManager
    function showAllComments()
    function addComment($comment) - the beginning of the function, miss
    session_user

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -2,10 +2,36 @@
     /**
      * Created by PhpStorm.
      * User: natacha
-     * Date: 14/04/2017
-     * Time: 18:06
+     * Date: 20/04/2017
+     * Time: 09:07
      */
     class CommentManager extends BddManager
     {
+        public function showAllComments()
+        {
+            $comments = array();
+            
+            $q = $this->_db->query('SELECT * FROM comments');
+            while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
+            {
+                $comments[] = new Comment($donnees);
+            }
+            return $comments;
+        }
         
+        public function addComment(Comment $comment)
+        {
+            $q = $this->_db->prepare('INSERT INTO comments (title, texte, datecreated, idUser, idchapter, commentsid) VALUES(:title, :texte, NOW(), 
+ :idUser, :idChapter, :commentsid)');
+            $q->bindValue(':title', $comment->getTitle());
+            $q->bindValue(':texte', $comment->getTexte());
+            $q->bindValue(':idUser', $comment->getIdUser());
+            $q->bindValue('idChapter', $comment->getIdchapter());
+            $q->bindValue('commentsid', $comment->getCommentsId());
+            $q->execute();
+            
+            $comment->hydrate([
+                'id' => $this->_db->lastInsertId()
+                              ]);
+        }
     }
