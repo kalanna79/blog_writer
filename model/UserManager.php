5086e1a60982d3ee9e8875a1b6dd5b0b32b155f2
kalanna79 <natacha@boudetnature.com>
2017-04-21 15:28:16 +0200

    add function addUser

--- model/UserManager.php
+++ model/UserManager.php
@@ -39,7 +39,39 @@
             return new User($donnees);
         }
         
-        /**
-         *
-         */
+        public function addUser(User $user)
+        {
+            $q = $this->_db->prepare('INSERT INTO user (firstname, name, pseudo, email, password, datemodified, roleiduser, datecreated) VALUES(:firstname,
+ :name, :pseudo, :email, :password, :datemodified, :roleiduser, NOW())');
+            $q->bindValue(':firstname', $user->getFirstName());
+            $q->bindValue(':name', $user->getName());
+            $q->bindValue(':pseudo', $user->getPseudo());
+            $q->bindValue(':email', $user->getEmail());
+            $q->bindValue(':password', $user->getPassword());
+            $q->bindValue(':datemodified', NULL);
+            $q->bindValue(':roleiduser', 2);
+            $q->execute();
+            
+            $user->hydrate([
+                'idUser' => $this->_db->lastInsertId()
+                           ]);
+        }
+        
+        public function verifUser()
+        {
+            $q = $this->_db->prepare('SELECT * FROM user WHERE pseudo=:pseudo AND password=:password');
+            $q->execute(array(
+                'pseudo' => $_POST['pseudo'], 'password' => $_POST['password']));
+            $resultat = $q->fetch(PDO::FETCH_ASSOC);
+            if (!$resultat)
+            {
+                echo 'Mauvais identifiant ou mot de passe';
+            }
+            else
+            {
+                session_start();
+                $_SESSION['id'] = $resultat['idUser'];
+                $_SESSION['pseudo'] = $resultat['pseudo'];
+            }
+        }
     }
