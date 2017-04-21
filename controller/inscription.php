093870a3be6201adb1a897043d5b6b807e11e550
kalanna79 <natacha@boudetnature.com>
2017-04-21 15:27:08 +0200

    create addUser via inscription form

--- controller/inscription.php
+++ controller/inscription.php
@@ -7,6 +7,29 @@
      */
     
     include('../config.php');
+    
+    $manager = new UserManager();
+    
+    //ajout d'un nouvel utilisateur (form Inscription)
+    
+    if (isset($_POST['inscription']) && isset($_POST['pseudo']))
+    {
+        $user = new User(['firstname' => $_POST['firstname'],
+                         'name' => $_POST['name'],
+                         'pseudo' => $_POST['pseudo'],
+                         'email' => $_POST['email'],
+                         'password' => $_POST['password']]);
+        $manager->addUser($user);
+        header('Location:'.HOST.'confirmation_inscription.php?id='.$user->getIdUser());
+    }
+    
+    //connexion d'un utilisateur déjà existant
+    if (isset($_POST['connect']))
+    {
+        $manager->verifUser();
+        header('Location:'.HOST.'profil_user.php?id='.$_SESSION['id']);
+    }
+    
     include (VIEW.'header.php');
     include (VIEW.'inscription.php');
     include (VIEW.'footer.php');
--- view/inscription.php
+++ view/inscription.php
@@ -5,8 +5,8 @@
         <form action="" method="post">
             <label for="pseudo">Pseudo</label> <br>
             <input type="text" name="pseudo"><br>
-            <label for="pwd">Mot de passe</label> <br>
-            <input type="text" name="pwd"><br>
+            <label for="password">Mot de passe</label> <br>
+            <input type="text" name="password"><br>
             <input type="checkbox" name="souvenir"> Se souvenir de moi <br><br>
             <input type="submit" name="connect" value="Se connecter">
         </form><br>
@@ -17,18 +17,19 @@
         <h3>S'inscrire</h3>
         <p>Inscrivez-vous pour déposer des commentaires et retrouvez votre avancée de lecture</p>
         <form action="" method="post">
-            <label for="prenom">Prénom</label> <br>
-            <input type="text" name="prenom"><br>
-            <label for="nom">Nom</label> <br>
-            <input type="text" name="nom"><br>
+            <label for="firstname">Prénom</label> <br>
+            <input type="text" name="firstname"><br>
+            <label for="name">Nom</label> <br>
+            <input type="text" name="name"><br>
             <label for="pseudo">Pseudo</label> <br>
             <input type="text" name="pseudo"><br>
             <label for="email">Email</label> <br>
             <input type="text" name="email"><br>
-            <label for="pwd">Mot de passe</label> <br>
-            <input type="text" name="pwd"><br>
-            <label for="pwd2">Répétez votre mot de passe</label> <br>
-            <input type="text" name="pwd2"><br><br>
+            <label for="password">Mot de passe</label> <br>
+            <input type="text" name="password"><br>
+            <!--<label for="pwd2">Répétez votre mot de passe</label> <br>
+            <input type="text" name="pwd2"> -->
+            <br><br>
             <input type="submit" name="inscription" value="Créer mon compte">
         </form>
     </section>
