5cf46bd4d43d6f196a814769c31738f0b4988987
kalanna79 <natacha@boudetnature.com>
2017-05-02 17:25:40 +0200

    delete old functions that have no utility

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -36,27 +36,11 @@
                               ]);
         }
         
-        public function showCommentsTree()
-        {
-            $q = $this->_db->query('SELECT * FROM comments');
-            $comments_by_id = array();
-            while ($donnees = $q->fetch(PDO::FETCH_ASSOC))
-            {
-                $comments_by_id[$donnees['id']] = array(
-                    'id' => $donnees['id'],
-                    'parent_id' =>$donnees['parentId'],
-                    'idUser' => $donnees['idUser'],
-					'titre' => $donnees['title'],
-                    'texte' => $donnees['texte'],
-					'date' => $donnees['datecreated'],
-                    'levelcomment' => $donnees['levelcomment']
-                );
-            }
-            return $comments_by_id;
-        }
-        
-        
-        public function hasChildren($id)
+        /**
+         * @param $id
+         * @return array
+         */
+        public function getChildren($id)
         {
             $children = array();
             
@@ -66,46 +50,6 @@
                     $children[] = new Comment($donnees);
                 }
                 return $children;
-            
         }
         
-    
-        public function afficher_commentaires($parent, $niveau, $array)
-        {
-            $html = "";
-            $niveau_precedent = 0;
-        
-            if (!$niveau && !$niveau_precedent)
-            {
-                $html .= "\n<ul>\n";
-            }
-        
-            foreach ($array AS $comment) {
-            
-                if ($parent == $comment['parent_id']) {
-                    if ($niveau_precedent < $niveau) {
-                        $html .= '<ul>';
-                    }
-                    $html .= "<li><span>" . $comment['idUser'] . " </span><span><strong>" . $comment['titre'] . '</strong></span><br><abbr>le ' . $comment['date'] . '</abbr><br>' . $comment['texte'];
-    
-                    //affichage des boutons répondre et signaler
-                    if ($niveau != 2) {
-                        $html .= '<button class="showForm btn btn-xs btn-success" id="comment-' . $comment['id'] . '"> Répondre</button>
-						<button class="showForm btn btn-xs btn-warning" id="comment-' . $comment['id'] . '"> Signaler</button>
-						<div class="row reponse" style="display: none" id="rep-comment-' . $comment['id'] . '" >
-							<form action="" method="post">
-								<input type="hidden" name="reponse" value=" ' . $comment['id'] . '">
-								<input type="hidden" name="level" value="1">
-								<textarea name="reponsetxt" cols="95" rows="3"></textarea> 						<br><br>
-								<input type="submit" value="Envoyer" name="submitreponse">
-							</form>
-						</div>';
-                    }
-    
-                }}}
-                  
-    
-    
-		 
-    
     }
