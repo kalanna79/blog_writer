058b303c6748332bdec679f0f77307fa07d2f360
kalanna79 <natacha@boudetnature.com>
2017-05-06 15:25:53 +0200

    add tinymce init

--- bootstrap/js/personal.js
+++ bootstrap/js/personal.js
@@ -34,7 +34,7 @@ $('#comment').click(function() {
 //hide response button for level 2 comments
 if ($('.level2')) {
     $('.alevel2').hide();
-};
+}
 
 //no ornated initial letter for the others pages than page 1
 
@@ -53,3 +53,19 @@ if ($('.level2')) {
         $("#chaptertext").removeClass("book");
         $("#chaptertext").addClass("book-nolettrine");
     }
+
+// move the alert box in the page
+ $('#inscription').append($('#alert'));
+
+tinymce.init({
+    selector: '.chapitre',
+    height: 500,
+    menubar: false,
+    plugins: [
+        'advlist autolink lists link image charmap print preview anchor',
+        'searchreplace visualblocks code fullscreen',
+        'insertdatetime media table contextmenu paste code'
+    ],
+    toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    content_css: '//www.tinymce.com/css/codepen.min.css'
+});
