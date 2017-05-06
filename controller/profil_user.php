466ba2e5c9f48aef0b9e942051ba7b6b9826cab3
kalanna79 <natacha@boudetnature.com>
2017-05-06 15:29:40 +0200

    add $sess and create condition if user is the admin or a user

--- controller/profil_user.php
+++ controller/profil_user.php
@@ -7,11 +7,27 @@
      */
     include('../config.php');
     
-    $sess = new Session();
+    
+    
     $manager = new UserManager();
     $user = $manager->getUserById($_SESSION['id']);
     
+    $sess = new Session();
+    $_SESSION['id'] = $user->getIdUser();
+    $_SESSION['pseudo'] = $sess->getPseudo();
+    
+    $users = $manager->AllUsers();
+    
+    $comanager = new CommentManager();
+    $comments = $comanager->showAllComments();
+    $lastcomments = $comanager->showLastComments();
+    
     
     include(ROOT . 'view/header.php');
-    include(VIEW . 'profil_user.php');
+    
+    if ($_SESSION['id'] == 1) {
+        include(VIEW . 'indexadmin.php');
+    } else {
+        include(VIEW . 'profil_user.php');
+    }
     include(VIEW . 'footer.php');
