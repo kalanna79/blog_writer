167082b3f9e37535eba06abe86b494242f9402cc
kalanna79 <natacha@boudetnature.com>
2017-05-10 13:12:30 +0200

    add buttons for modify chapters

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: natacha
+     * Date: 09/05/2017
+     * Time: 07:42
+     */
+    
+    /* index doit récupérer toutes les adresses pour rediriger sur l'accueil du site
+    */
+    require_once ('config.php');
+    include(HOST . 'index.php');
+    
--- model/Moderation.php
+++
@@ -1,7 +0,0 @@
-<?php
-    /**
-     * Created by PhpStorm.
-     * User: natacha
-     * Date: 09/05/2017
-     * Time: 15:57
-     */
