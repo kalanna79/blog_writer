827ddd8d7b347c715771ef505ad2243ab7733cd2
kalanna79 <natacha@boudetnature.com>
2017-05-11 20:36:55 +0200

    test with another class Mail
    class made by Kevin - exercise by Sandy

--- controller/contact.php
+++ controller/contact.php
@@ -8,22 +8,32 @@
     include ('../config.php');
     $manager = new UserManager();
     $auteur = $manager->getUserById(1);
-
     
     
     include (VIEW.'header.php');
+    
     if (isset($_POST['envoyer']))
     {
         if (!empty($_POST['mail']) && !empty($_POST['message']))
         {
+            $donnees = [
+                'expediteurmail' => $_POST['mail'],
+                'nomexp' => $_POST['nom'],
+                 'adresseMail' => $auteur->getEmail(),
+                'objet' => $_POST['sujet'],
+                'message' => $_POST['message']
+                               ];
             
+            $mail = new GestionnaireMail($donnees);
+            if ($mail->envoyerMail())
+            {
+                include(VIEW . 'confirmation_mail.php');
+            };
             
-            if ( ) {
-                include (VIEW . 'confirmation_mail.php');
-            }
         }
     } else {
-        include (VIEW.'contact.php');
+    
+        include(VIEW . 'contact.php');
     }
     
     include (VIEW.'footer.php');
--- view/confirmation_mail.php
+++ view/confirmation_mail.php
@@ -2,6 +2,9 @@
     <section class="col-md-12 presentation content">
         <h3>Votre message a bien été envoyé</h3>
 		<div class="message">J'ai bien reçu votre message et j'y répondrai dans les plus brefs délais ! <br> Merci de votre intérêt pour mon nouveau livre !</div>
+		<div class="message">Voici votre message : <br>
+		<?php echo $mail->getNomexp() . "(" . $mail->getExpediteurmail() . ")<br>Message : " . $mail->getMessage();?></div>
+		
 		<div class="message"><a href="<?php echo HOST . 'index.php';?>">Retour à l'accueil</a></div>
     
     </section>
